61003cf5059a14814e9218255639b9c216e6091a
FitriQusyairi <fitri.qusyairi96@gmail.com>
2021-05-09 13:44:34 +0800

    v2 updated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+Vue.use(Vuesax, {
+  //options here
+})
