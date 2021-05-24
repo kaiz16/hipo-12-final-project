170189e72901b4f745456a04589e47113c3f940a
MJ Munar <mjmunar.dev@gmail.com>
2021-05-24 20:17:56 +0800

    [Update] changed naming. Add matching routes

--- backend/routes/pets.js
+++ backend/routes/pets.js
@@ -3,21 +3,20 @@ const express = require("express");
 const router = express.Router();
 const Pets = require("../Models/Pets.js");
 const Personalities = require("../Models/Personalities.js")
+const Matches = require("../Models/Matches.js")
 const { ensureAuthenticated } = require("./auth.js");
-var pets = null;
-var petMatchesIds = []
 
 router.get("/pets", ensureAuthenticated, async (req, res) => {
-  pets = await Pets.find({});
+  let pets = await Pets.find({});
   res.json(pets);
 });
 
 router.get("/personalities", ensureAuthenticated, async (req, res) => {
-  pets = await Personalities.find({});
+  let pets = await Personalities.find({});
   res.json(pets);
 });
 
-router.get("/pets/matches", ensureAuthenticated, async (req, res) => {
+router.get("/pets/potential_matches", ensureAuthenticated, async (req, res) => {
 
   let { petId } = req.body;
   // Get pet's personality model by id 
@@ -31,12 +30,24 @@ router.get("/pets/matches", ensureAuthenticated, async (req, res) => {
 
   // get matches per each personality
   // TODO ADD FILTER TO GROUP SAME IDS
+  let petMatchesIds = [];
   for(let i = 0; i < personalities.length; i++){
     let ids = await getIdByPersonality(personalities[i], petId)
     //comment out if statement if you want to push nulls on each personality
     if(ids.length != 0) petMatchesIds.push(ids)
   }
   res.json(petMatchesIds);
+});
+
+router.get("/pets/matches", ensureAuthenticated, async (req, res) => {
+
+  let { petId } = req.body;
+
+  let petMatchesIds = await Matches.find({
+    petIdUser: petId
+  })
+
+  res.json(petMatchesIds)
 });
 
 router.post("/create", ensureAuthenticated, async (req, res) => {
@@ -51,7 +62,7 @@ router.post("/create", ensureAuthenticated, async (req, res) => {
   res.json(pet);
 });
 
-router.post("/pets/personality", ensureAuthenticated, async (req, res) => {
+router.post("/create/personality", ensureAuthenticated, async (req, res) => {
   console.log(req.body)
   let { id, personality } = req.body;
   let newPersonality = new Personalities({
@@ -62,6 +73,20 @@ router.post("/pets/personality", ensureAuthenticated, async (req, res) => {
   await newPersonality.save();
 
   res.json(newPersonality);
+});
+
+router.post("/create/match", ensureAuthenticated, async (req, res) => {
+  console.log(req.body)
+  let { petId, matchId, personality } = req.body;
+  let newMatch = new Matches({
+    petIdUser: petId,
+    petIdMatch: matchId,
+    personalityType: personality
+  })
+
+  await newMatch.save();
+
+  res.json(newMatch)
 });
 
 router.delete("/delete", ensureAuthenticated, async (req, res) => {
