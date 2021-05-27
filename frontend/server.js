3fc6b52cc6807cf4b9cca9f0c4bde08bdfab51e3
kaiz16 <realkaiz7@gmail.com>
2021-05-28 00:26:50 +0800

    frontend: deploy

---
+++ frontend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const connectHistoryApiFallback = require('connect-history-api-fallback')
+// server static library
+// this will serve index.html
+const serveStatic = require("serve-static")
+// 
+const path = require('path');
+
+// init express
+const app = express();
+
+app.use(connectHistoryApiFallback())
+// get the folder path
+
+const folderPath = path.join(__dirname, 'dist')
+
+// index.html
+const rootFile = serveStatic(folderPath)
+
+app.use(rootFile);
+
+const port = process.env.PORT || 8080;
+app.listen(port);
